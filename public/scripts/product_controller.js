baeee6ed6d0fea197a2d4497cc3bd63b1e320ed1
ShrikeMountsTech <shrikemounts@gmail.com>
2016-12-10 18:01:17 +0000

    changed image on homescreen and order of carousel

--- public/scripts/product_controller.js
+++ public/scripts/product_controller.js
@@ -5,26 +5,26 @@ shrikeApp.controller('product_controller', function($scope) {
     $scope.slides = [];
     
     var slide1 = {
-        quote: "IT WAS GREAT HAVING \n A PLACE TO PUT A BOTTLE \n OF WATER",
-        default_image_path: "../images/gallery/OEIP_Spin-In-Motorcycle-012 default.jpg", 
-        high_res_image_path: "../images/gallery/OEIP_Spin-In-Motorcycle-012.jpg",
-        rider_name: "IndyRider \n Ninja 300" 
-    };
-    $scope.slides.push(slide1);
-    
-    var slide2 = {
         quote: "INSTALLATION WAS AN \n ABSOLUTE BREEZE.",
         default_image_path: "../images/gallery/OEIP_Spin-In-Motorcycle-028 default.jpg", 
         high_res_image_path: "../images/gallery/OEIP_Spin-In-Motorcycle-028.jpg",
         rider_name: "Chase \n GSXR750" 
     };
-    $scope.slides.push(slide2);
+    $scope.slides.push(slide1);
     
-    var slide3 = {
+    var slide2 = {
         quote: "THIS IS EXACTLY WHAT \n I'VE WANTED, BUT HAVE NOT \n BEEN ABLE TO FIND!",
         default_image_path: "../images/gallery/OEIP_Spin-In-Motorcycle-016 default.jpg", 
         high_res_image_path: "../images/gallery/OEIP_Spin-In-Motorcycle-016.jpg",
         rider_name: "Jamie \n GSXR750"
+    };
+    $scope.slides.push(slide2);
+    
+    var slide3 = {
+        quote: "IT WAS GREAT HAVING \n A PLACE TO PUT A BOTTLE \n OF WATER",
+        default_image_path: "../images/gallery/OEIP_Spin-In-Motorcycle-012 default.jpg", 
+        high_res_image_path: "../images/gallery/OEIP_Spin-In-Motorcycle-012.jpg",
+        rider_name: "IndyRider \n Ninja 300" 
     };
     $scope.slides.push(slide3);
     
